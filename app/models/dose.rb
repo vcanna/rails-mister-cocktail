c25c0f0a0ff98a178821a0516144be3660ce0442
victor cannavale <victorcannavale@gmail.com>
2020-09-18 16:07:11 -0300

    teste

---
+++ app/models/dose.rb
@@ -0,0 +1,10 @@
+class Dose < ApplicationRecord
+    
+    belongs_to :cocktail
+    
+    belongs_to :ingredient
+
+    validates :description, presence: true
+    validates :cocktail, uniqueness: {scope: :ingredient}
+    
+end
