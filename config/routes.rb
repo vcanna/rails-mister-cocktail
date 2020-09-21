8be13961164dd4295cbede257863ce0c4b054e63
victor cannavale <victorcannavale@gmail.com>
2020-09-21 12:04:56 -0300

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "cocktails#index"
 
   resources :cocktails, only: [ :index, :show, :new, :create ] do 
-    resources :doses, only: [ :create ]
+    resources :doses, only: [ :new, :create ]
   end
 
   resources :doses, only: [ :destroy ] 
